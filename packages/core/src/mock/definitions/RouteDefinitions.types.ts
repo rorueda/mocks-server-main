36a4a2205e397fd2a947ed9f07c5c6cd8024b145
rrueda <rorueda@gmail.com>
2025-01-30 14:46:46 -0300

    Revert "fix typescript declaration for disabled variant"
    This reverts commit 4b3f1c8c792c844a5d5205004f6cdb2bd1a4f234.

--- packages/core/src/mock/definitions/RouteDefinitions.types.ts
+++ packages/core/src/mock/definitions/RouteDefinitions.types.ts
@@ -23,15 +23,10 @@ declare global {
     interface VariantDefinitionCommon {
       /** Route variant id */
       id: VariantDefinitionId;
+      /** Variant is disabled */
+      disabled?: boolean;
       /** Delay to apply to the response */
       delay?: number;
-    }
-
-    interface VariantDefinitionDisabled {
-      /** Route variant id */
-      id: VariantDefinitionId;
-      /** Variant is disabled */
-      disabled: true;
     }
 
     /** Different variant properties by variant handler id */
@@ -45,9 +40,8 @@ declare global {
     type VariantHandlerTypes = keyof VariantHandlerOptionsByType;
 
     /** Route variant definition */
-    type VariantDefinition =
-      | VariantDefinitionDisabled
-      | (VariantHandlersDefinitions[keyof VariantHandlersDefinitions] & VariantDefinitionCommon);
+    type VariantDefinition = VariantHandlersDefinitions[keyof VariantHandlersDefinitions] &
+      VariantDefinitionCommon;
 
     type VariantHandlerTypeOptions =
       VariantHandlerOptionsByType[keyof VariantHandlerOptionsByType];
